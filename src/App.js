1960cd8b2812c72ac7bee79f1e7c0046c19f9f01
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 09:39:27 +0200

    updating airtable base to match reuse directory

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import './App.css';
 
 function App() {
   var Airtable = require('airtable');
-  var base = new Airtable({apiKey: 'keyEgsODRGeMoFEqh'}).base('appINu5zQbTPii8YK');
+  var base = new Airtable({apiKey: 'keyEgsODRGeMoFEqh'}).base('app71fe0Ff06gsUXD');
   const [data, setData] = React.useState([])
 
-  React.useEffect(() => {
-    base('test').select({
-        maxRecords: 1000,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            setData(prev => [...prev, record.get('name')]);
-            console.log(data);
-        });
-        fetchNextPage();
+  // React.useEffect(() => {
+  //   base('test').select({
+  //       maxRecords: 1000,
+  //       view: "Grid view"
+  //   }).eachPage(function page(records, fetchNextPage) {
+  //       records.forEach(function(record) {
+  //           setData(prev => [...prev, record.get('name')]);
+  //       });
+  //       fetchNextPage();
 
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-  }, []) 
+  //   }, function done(err) {
+  //       if (err) { console.error(err); return; }
+  //   });
+  // }, []) 
   
   return (
     <div className="App">
