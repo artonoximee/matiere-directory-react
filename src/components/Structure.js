008afa704e15b5fc268f0d8d51a05b09992a6293
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 12:50:49 +0200

    finished conditional rendering on structure cards

--- src/components/Structure.js
+++ src/components/Structure.js
@@ -16,29 +16,53 @@ function Structure(props) {
     instagram_url
   } = props.structure
 
+  function urlBeautifier(url) {
+    if (url.slice(0, 8) === "https://") {
+      return url.slice(8, -1)
+    } else {
+      return url.slice(7, -1)
+    }
+  }
+
   return (
     <div className="card reveal border border-1 border-secondary text-bg-dark mt-4">
       <div className="card-body">
         <div className="row align-items-end mt-2">
           <div className="col-md-6 col-sm-12">
-            <h3><b>{name}</b></h3>
+            {
+              name &&
+              <h3><b>{name}</b></h3>
+            }
           </div>
           <div className="col-md-3 col-sm-12">
-            <h2 className="badge text-bg-light me-2">{structure_types}</h2>
+            {
+              structure_types &&
+              <h2 className="badge text-bg-light me-2">{structure_types}</h2>
+            }
           </div>
           <div className="col-md-3 col-sm-12 text-md-end text-sm-start">
-            <h5>{postcode}, {city}</h5>
+            {
+              postcode && city &&
+              <h5>{postcode}, {city}</h5>
+            }
           </div>
         </div>
-        <p className="text-bg-dark mt-2">
-          {description}
-        </p>
+        {
+          description &&
+          <p className="text-bg-dark mt-2">{description}</p>
+        }
         <div className="row">
           <div className="col-lg-6 col-md-12 mt-2">
-            <a className="btn btn-sm btn-outline-light" target="_blank" href={website} rel="noreferrer"><i className="fa-solid fa-globe"></i> {website}</a>
+            {
+              website &&
+              <a className="btn btn-sm btn-outline-light" target="_blank" href={website} rel="noreferrer"><i className="fa-solid fa-globe"></i> {urlBeautifier(website)}</a>
+            }
           </div>
           <div className="col-lg-4 col-md-12 mt-2">
-            <a className="btn btn-sm btn-outline-light" href={`mailto:${email}`}><i className="fa-solid fa-envelope"></i> {email}</a>
+            {
+              email &&
+              <a className="btn btn-sm btn-outline-light" href={`mailto:${email}`}><i className="fa-solid fa-envelope"></i> {email}</a>
+            }
           </div>
           <div className="col-lg-2 col-md-12 mt-2 text-lg-end text-md-start">
             {
@@ -56,16 +80,21 @@ function Structure(props) {
             
           </div>
         </div>
-        { 
-          address && 
+        
           <div className="row">
             <div className="col-lg-6 col-md-12 mt-3">
-              <span><i className="fa-solid fa-location-dot"></i> {address}, {postcode}, {city}</span>
+              { 
+                address && 
+                <span><i className="fa-solid fa-location-dot"></i> {address}, {postcode}, {city}</span>
+              }
             </div>
             <div className="col-lg-6 col-md-12 mt-3">
+              {
+                telephone &&
+                <span><i class="fa-solid fa-phone"></i> {telephone}</span>
+              }
             </div>
           </div>
-        }
       </div>
     </div>
   )
