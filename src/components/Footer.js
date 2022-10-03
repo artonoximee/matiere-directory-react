41abb348ac6459c3b38bb524d7bbc744690d2bcf
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 12:02:30 +0200

    updated rel attribute on a tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer text-bg-dark border-top border-secondary text-center">
       <p>
-        <a href="https://github.com/artonoximee/matiere-directory-js" className="btn btn-sm btn-outline-light" target="_blank"><i className="fa-brands fa-github"></i></a> <a href="mailto:contact@mlav.land" className="btn btn-sm btn-outline-light">Contact</a>
+        <a href="https://github.com/artonoximee" className="btn btn-sm btn-outline-light" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a> <a href="mailto:contact@mlav.land" className="btn btn-sm btn-outline-light">Contact</a>
       </p>
     </div>
   )
