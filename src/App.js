050ce381e470ca50a5f1b42a940c8a68482e9ccf
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 11:04:50 +0200

    successfully displaying departments in the departments selector

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   
   return (
     <div className="App">
-      {JSON.stringify(departments)}
+      <Form departments={departments} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,15 @@ function Form(props) {
   const departments = props.departments;
 
   const optionsDpt = departments.map(dpt => 
-    <p key={dpt.id} value={dpt.id}>{dpt.num} - {dpt.name}</p>
+    <option key={dpt.id} value={dpt.id}>{dpt.num} - {dpt.name}</option>
   )
 
   return (
     <div className="form">
       <select id="department" className="form-select form-select-lg mb-3">
         <option value="ALL">Tous les départements</option>
-      </select>
         {optionsDpt}
+      </select>
     </div>
   )
 }
