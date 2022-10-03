ed6135b4e4aef7c5ce110cdb9b58fef9963c4378
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 09:36:57 +0200

    successfully retrieving data from airtable and displaying it while loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: 'keyEgsODRGeMoFEqh'}).base('appINu5zQbTPii8YK');
+  const [data, setData] = React.useState([])
+
+  React.useEffect(() => {
+    base('test').select({
+        maxRecords: 1000,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            setData(prev => [...prev, record.get('name')]);
+            console.log(data);
+        });
+        fetchNextPage();
+
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+  }, []) 
+  
   return (
     <div className="App">
-      <p>Hello</p>
+      <p>{data}</p>
     </div>
   );
 }
