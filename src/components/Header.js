c006e985f06fd6d3129dfb903524e5931c29235b
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-03 12:05:37 +0200

    added text to remember to display structures count

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <h1 className="text-center"><i className="fa-solid fa-database text-secondary"></i> &nbsp;Annuaire Réemploi</h1>
         <p className="fs-4 mt-5 text-center">
           L'objectif de cette base de données est de répertorier <b>les acteur·ices qui œuvrent pour le réemploi de matériaux de construction en France</b>.
-          À ce jour, structures sont référencées. L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
+          À ce jour, <span className="text-danger">#</span> structures sont référencées. L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
         </p>
         
       </div>
