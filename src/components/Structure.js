9b93afb4b94fa84d36f4fb3ad5ea5e2b8648621c
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-04 18:41:33 +0200

    reorganized conditional rendering in Structure component

--- src/components/Structure.js
+++ src/components/Structure.js
@@ -51,45 +51,53 @@ function Structure(props) {
               <a className="btn btn-sm btn-outline-light" target="_blank" href={website} rel="noreferrer"><i className="fa-solid fa-globe text-success"></i> {urlBeautifier(website)}</a>
             }
           </div>
-          <div className="col-12 mt-2">
-            {
-              email &&
-              <a className="btn btn-sm btn-outline-light" href={`mailto:${email}`}><i className="fa-solid fa-envelope text-success"></i> {email}</a>
-            }
-          </div>
-          <div className="col-12 mt-2">
-            {
-              facebook_url &&
-              <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={facebook_url} rel="noreferrer"><i className="fa-brands fa-facebook text-success"></i></a>
-            }
-            {
-              instagram_url && 
-              <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={instagram_url} rel="noreferrer"><i className="fa-brands fa-instagram text-success"></i></a>
-            }
-            {
-              twitter_url && 
-              <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={twitter_url} rel="noreferrer"><i className="fa-brands fa-twitter text-success"></i></a>
-            }
-            
-          </div>
+          
+          {
+            (facebook_url || instagram_url || twitter_url || email) && 
+            <>
+              <div className="col-8 mt-2">
+                {
+                  email &&
+                  <a className="btn btn-sm btn-outline-light" href={`mailto:${email}`}><i className="fa-solid fa-envelope text-success"></i> {email}</a>
+                }
+              </div>
+              <div className="col-4 mt-2 text-end">
+                {
+                  facebook_url &&
+                  <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={facebook_url} rel="noreferrer"><i className="fa-brands fa-facebook text-success"></i></a>
+                }
+                {
+                  instagram_url && 
+                  <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={instagram_url} rel="noreferrer"><i className="fa-brands fa-instagram text-success"></i></a>
+                }
+                {
+                  twitter_url && 
+                  <a className="btn btn-sm btn-outline-light me-2" target="_blank" href={twitter_url} rel="noreferrer"><i className="fa-brands fa-twitter text-success"></i></a>
+                }
+              </div>
+            </>
+          }
         </div>
 
-        
-        
+        {
+          (address || telephone) &&
           <div className="row">
+            <div className="col-8 mt-3">
               { 
                 address && 
-                <div className="col-8 mt-3">
-                  <a className="btn btn-sm btn-outline-light" target="_blank" href={`https://www.google.com/maps?q=${address}+${postcode}+${city}}`} rel="noreferrer"><i className="fa-solid fa-location-dot text-success"></i> {address}, {postcode}, {city}</a>
-                </div>
+                <a className="btn btn-sm btn-outline-light" target="_blank" href={`https://www.google.com/maps?q=${address}+${postcode}+${city}}`} rel="noreferrer"><i className="fa-solid fa-location-dot text-success"></i> {address}, {postcode}, {city}</a>
               }
+            </div>
+            <div className="col-4 mt-3 text-end">
               {
                 telephone &&
-                <div className="col-4 mt-3">
-                  <a className="btn btn-sm btn-outline-light" href={`tel:${telephone}}`}><i className="fa-solid fa-phone text-success"></i> {telephone}</a>
-                </div>
+                <a className="btn btn-sm btn-outline-light" href={`tel:${telephone}}`}><i className="fa-solid fa-phone text-success"></i> {telephone}</a>
               }
+            </div>
           </div>
+        }
+        
+        
       </div>
     </div>
   )
