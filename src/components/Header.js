29e8c3a63f91ec1c1f407bb860f7f1ead06938c0
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-04 18:23:50 +0200

    removed counter in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
         <h1 className="text-center"><i className="fa-solid fa-address-book text-success"></i> &nbsp;Annuaire Réemploi</h1>
         <p className="fs-4 mt-5 text-center">
           L'objectif de cette base de données est de répertorier <b>les acteur·ices qui œuvrent pour le réemploi de matériaux de construction en France</b>.
-          À ce jour, <span className="fw-bold">{props.structuresCount}</span> structures sont référencées. L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
+          L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
         </p>
         
       </div>
