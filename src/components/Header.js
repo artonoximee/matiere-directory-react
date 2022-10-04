ad5f22d7ce5d0ae3e670bf3ded39d7b34396de2f
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-04 17:42:14 +0200

    updated favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
   return (
     <div className="row mt-5 mb-5 justify-content-center">
       <div className="col mt-5">
-        <h1 className="text-center"><i className="fa-solid fa-database text-success"></i> &nbsp;Annuaire Réemploi</h1>
+        <h1 className="text-center"><i className="fa-solid fa-address-book text-success"></i> &nbsp;Annuaire Réemploi</h1>
         <p className="fs-4 mt-5 text-center">
           L'objectif de cette base de données est de répertorier <b>les acteur·ices qui œuvrent pour le réemploi de matériaux de construction en France</b>.
           À ce jour, <span className="fw-bold">{props.structuresCount}</span> structures sont référencées. L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
