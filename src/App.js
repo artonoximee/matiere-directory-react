77fb22e70dd363b3fa5772e9aa1adf73028e26e8
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-04 16:46:20 +0200

    added component Loader

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Form from './components/Form'
 import Structure from './components/Structure'
+import Loader from './components/Loader'
 import './App.css';
 
 function App() {
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components/Loader.css'
+
+function Loader() {
+  return (
+    <div className="loader">
+      
+    </div>
+  )
+}
+
+export default Loader;
