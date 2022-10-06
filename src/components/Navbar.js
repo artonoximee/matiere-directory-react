1dc01c7194dd89823b649068b07e12936a78ede8
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-06 17:31:12 +0200

    removed footer, removed Navbar.css and added fixed-top class instead

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-dark p-4 border-bottom border-secondary">
+    <nav className="navbar fixed-top navbar-dark navbar-expand-lg bg-dark p-4 border-bottom border-secondary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#"><i className="fa-solid fa-address-book text-success"></i> &nbsp;Annuaire Réemploi</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav ms-5 me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-5 me-auto">
           <li className="nav-item">
               <a className="nav-link" href="#">Accueil</a>
             </li>
@@ -21,7 +20,7 @@ function Navbar() {
               <a className="nav-link" href="#">Ajouter</a>
             </li>
           </ul>
-          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link" href="#">Contact</a>
             </li>
