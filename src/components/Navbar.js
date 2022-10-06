4557531804652b97e92f51ebf7258dc6ba78a051
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-06 19:50:12 +0200

    added Link components to replace a tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar fixed-top navbar-dark navbar-expand-lg bg-dark p-4 border-bottom border-secondary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/"><i className="fa-solid fa-address-book text-success"></i> &nbsp;Annuaire Réemploi</a>
+        <Link to="/" className="navbar-brand"><i className="fa-solid fa-address-book text-success"></i> &nbsp;Annuaire Réemploi</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-            <a className="nav-link" href="/">Recherche</a>
+              <Link to="/" className="nav-link">Recherche</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/form">Proposer un ajout</a>
+              <Link to="form" className="nav-link">Proposer un ajout</Link>
             </li>
           </ul>
           <ul className="navbar-nav ml-auto">
