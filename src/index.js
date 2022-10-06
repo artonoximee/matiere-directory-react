28fea067555bec50483ff9ce10e494c43a80758a
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-06 19:47:48 +0200

    modified router logic to match react router organisation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider, Link} from "react-router-dom";
+
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
+import Search from './components/Search'
+import ProposalForm from './components/ProposalForm'
 import reportWebVitals from './reportWebVitals';
+
+// handle routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Search />
+      },
+      {
+        path: "form",
+        element: <ProposalForm />
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
