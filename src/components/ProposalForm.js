c851ff0894df20c76278124901303ed93730d4f6
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-06 19:56:18 +0200

    started proposal form

--- src/components/ProposalForm.js
+++ src/components/ProposalForm.js
@@ -3,9 +3,19 @@ import React from "react";
 function ProposalForm() {
   return (
     <>
-      <p className="mt-5">MY FORM GOES HERE</p>
+      <div className="row justify-content-center top-margin">
+        <div className="col-12">
+          <input 
+            type="text" 
+            className="form-control form-control-lg mb-3 bg-dark border-secondary text-light"
+            placeholder="Rechercher par nom"
+            value={null}
+            name="text"
+            onChange={null}
+          />
+        </div>
+      </div>
     </>
-   
   )
 }
 
