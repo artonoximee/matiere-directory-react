d89190f9d433bd589843ee09115f77e3dbc69c7f
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-07 16:22:02 +0200

    modified one word on ContactHeader

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -6,7 +6,7 @@ function ContactHeader() {
       <div className="col mt-5">
         <h1 className="text-center"><i className="fa-solid fa-paper-plane text-success"></i> &nbsp;Nous contacter</h1>
         <p className="fs-4 mt-5 text-center">
-          Si vous souhaitez nous contacter pour suggérer des modifications, demander l'enlèvement de vos coordonnées de l'annuaire, ou simplement si vous souhaitez nous passer le bonjour.
+          Si vous souhaitez nous contacter pour suggérer des modifications, demander la suppression de vos coordonnées de l'annuaire, ou simplement si vous souhaitez nous passer le bonjour.
         </p>
       </div>
     </div>
