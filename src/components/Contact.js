b9c9da9820288e6f355c4faebf98ff0a062b6e1b
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-07 17:18:43 +0200

    finished contact form using react-hook-form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,60 +4,10 @@ import ContactHeader from "./ContactHeader"
 import ContactForm from "./ContactForm"
 
 function Contact() {
-  // initializing airtable and states
-  var Airtable = require('airtable');
-  var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base(process.env.REACT_APP_AIRTABLE_BASE);
-  
-  const [formData, setFormData] = useState(
-    {
-      name: "",
-      email: "",
-      message: ""
-    }
-  );
-
-  function handleChange(e) {
-    const {name, value} = e.target;
-    setFormData(prevFormData => {
-      return {
-        ...prevFormData,
-        [name]: value
-      }
-    })
-  }
-
-  function handleSubmit() {
-    base('contacts').create([
-      {
-        "fields": {
-          "name": formData.name,
-          "email": formData.email,
-          "message": formData.message
-        }
-      }
-    ], function(err, records) {
-      if (err) {
-        return;
-      }
-      setFormData(
-        {
-          name: "",
-          email: "",
-          message: ""
-        }
-      )
-    });
-  }
-
-
   return (
     <>
       <ContactHeader />
-      <ContactForm 
-        formData={formData} 
-        handleChange={(e) => handleChange(e)}
-        handleSubmit={handleSubmit}
-      />
+      <ContactForm />
     </>
   )
 }
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,46 +1,70 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
 function SuggestForm(props) {
-  const {name, email, message} = props.formData
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base(process.env.REACT_APP_AIRTABLE_BASE);
+  
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+  function onSubmit(data) {
+    base('contacts').create([
+      {
+        "fields": {
+          "name": data.name,
+          "email": data.email,
+          "message": data.message
+        }
+      }
+    ], function(err, records) {
+      if (err) {
+        return;
+      }
+    });
+    reset( {
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
 
   return (
     <>
       <div className="row justify-content-center mt-4">
         <div className="col-12">
-          <label htmlFor="name" className="form-label"><i className="fa-solid fa-user text-success"></i> Votre nom</label>
+          <label htmlFor="name" className="form-label mt-3"><i className="fa-solid fa-user text-success"></i> Votre nom</label>
           <input 
             type="text"
             id="name"
-            className="form-control form-control-lg mb-3 bg-dark border-secondary text-light"
+            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.name && "is-invalid border-danger"}`}
             placeholder="Marcel Dupont"
-            value={name}
-            name="name"
-            onChange={props.handleChange}
+            {...register("name", { required: true })}
           />
-          <label htmlFor="name" className="form-label"><i className="fa-solid fa-envelope text-success"></i> Votre email</label>
+          {errors.name && <p className="text-danger mt-2">Il semblerait que vous n'ayez pas renseigné de nom</p>}
+
+          <label htmlFor="name" className="form-label mt-3"><i className="fa-solid fa-envelope text-success"></i> Votre email</label>
           <input 
-            type="text"
+            type="email"
             id="name"
-            className="form-control form-control-lg mb-3 bg-dark border-secondary text-light"
+            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.email && "is-invalid border-danger"}`}
             placeholder="marceldupont@gmail.com"
-            value={email}
-            name="email"
-            onChange={props.handleChange}
+            {...register("email", { required: true, pattern: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i })}
           />
-          <label htmlFor="message" className="form-label"><i className="fa-solid fa-file-lines text-success"></i> Votre message</label>
+          {errors.email && <p className="text-danger mt-2">Adresse email invalide, merci de la vérifier</p>}
+
+          <label htmlFor="message" className="form-label mt-3"><i className="fa-solid fa-file-lines text-success"></i> Votre message</label>
           <textarea 
             type="text"
             id="message"
             rows="5"
-            className="form-control form-control-lg mb-5 bg-dark border-secondary text-light"
+            className={`form-control form-control-lg bg-dark border-secondary text-light ${errors.message && "is-invalid border-danger"}`}
             placeholder="Votre message ici !"
-            value={message}
-            name="message"
-            onChange={props.handleChange}
+            {...register("message", { required: true })}
           />
+          {errors.message && <p className="text-danger mt-2">Il serait préférable d'ajouter un petit message, non ?</p>}
 
-          <div className="d-grid gap-2 mb-5 bottom-margin">
-            <button className="btn btn-lg btn-outline-success" onClick={null} type="submit" disabled={!props.disableSubmit}>Envoyer</button>
+          <div className="d-grid gap-2 mt-5 bottom-margin">
+            <button className="btn btn-lg btn-outline-success" onClick={handleSubmit(onSubmit)} type="submit">Envoyer</button>
           </div>
         </div>
       </div>
