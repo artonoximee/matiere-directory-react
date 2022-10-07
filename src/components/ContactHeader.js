11c47dd570e4e8a889987ec29651de7bb3c41b4c
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-07 10:29:15 +0200

    updated contents on headers

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -6,7 +6,7 @@ function ContactHeader() {
       <div className="col mt-5">
         <h1 className="text-center"><i class="fa-solid fa-paper-plane text-success"></i> &nbsp;Nous contacter</h1>
         <p className="fs-4 mt-5 text-center">
-          Si vous souhaitez nous contacter pour suggérer des modifications, demander l'enlèvement de vos coordonnées de l'annuaire, ou n'importe quelle raison <i class="fa-solid fa-face-smile-wink text-success"></i>
+          Si vous souhaitez nous contacter pour suggérer des modifications, demander l'enlèvement de vos coordonnées de l'annuaire, ou simplement si vous souhaitez nous passer le bonjour.
         </p>
       </div>
     </div>
--- src/components/SuggestHeader.js
+++ src/components/SuggestHeader.js
@@ -6,8 +6,7 @@ function SuggestHeader() {
       <div className="col mt-5">
         <h1 className="text-center"><i className="fa-solid fa-square-plus text-success"></i> &nbsp;Proposer un ajout à l'annuaire</h1>
         <p className="fs-4 mt-5 text-center">
-          L'objectif de cette base de données est de répertorier <b>les acteur·ices qui œuvrent pour le réemploi de matériaux de construction en France</b>.
-          L'outil de recherche ci-dessous vous permet d'accéder aux acteur·ices autour de vous selon les services qu'ielles proposent.
+          Si vous connaissez une structure qui entre dans les critères de l'annuaire réemploi et qui n'y figure pas encore, vous pouvez ici nous envoyer votre suggestion. Elle sera validée dans les plus brefs délais avant d'être publiée.
         </p>
       </div>
     </div>
