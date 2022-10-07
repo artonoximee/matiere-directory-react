cf5cf41091d6a9d52d1de10b760127950161a9b6
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-10-07 08:28:13 +0200

    finished error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,22 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+import Navbar from "./Navbar";
 
 function ErrorPage() {
   return(
-    <h1>Oups</h1>
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row mt-5 justify-content-center top-margin">
+          <div className="col-lg-8 col-md-12 p-5 text-center">
+            <h1><i class="fa-solid fa-triangle-exclamation text-secondary"></i></h1>
+            <h2>Oops, la page que vous recherchez n'existe pas</h2>
+            <Link to="/" className="btn btn-outline-success mt-5">Revenir à l'accueil ?</Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
